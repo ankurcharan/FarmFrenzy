4883ae18bb1701e7be1095054ac5d33fbbf2b1ef
Ankur Charan <ankurcharan98@gmail.com>
2020-01-21 09:37:50 +0530

    new frontend
    Signed-off-by: Ankur Charan <ankurcharan98@gmail.com>

---
+++ public/precache-manifest.ab6a87daaa32c5d9f9ca5b77e6c4ba44.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e07c18b70cd1ea82b8d55345ecda460b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f0b03bab37b4bb0d3b8",
+    "url": "/static/css/main.5db34fa0.chunk.css"
+  },
+  {
+    "revision": "cf6cb36c2dc72b795399",
+    "url": "/static/js/2.ac1c1b4d.chunk.js"
+  },
+  {
+    "revision": "ad70875a8e85d7b18a89b606b3ec32af",
+    "url": "/static/js/2.ac1c1b4d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f0b03bab37b4bb0d3b8",
+    "url": "/static/js/main.9d0212e1.chunk.js"
+  },
+  {
+    "revision": "cef183ef4963e83bf9a0",
+    "url": "/static/js/runtime-main.351877cc.js"
+  }
+]);
