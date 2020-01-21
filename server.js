9e4541aa02a69c053b1b3498735ee99f3771054b
Ankur Charan <ankurcharan98@gmail.com>
2020-01-21 09:30:30 +0530

    errors
    Signed-off-by: Ankur Charan <ankurcharan98@gmail.com>

--- server.js
+++ server.js
@@ -205,6 +205,29 @@ app.post('/api/expressup', (req, res) => {
 
 app.get('/api/crops', (req, res) => {
 
+	const cropData = {
+		wheat: {
+			name: 'Wheat',
+			text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+		},
+		coffee: {
+			name: 'Coffee',
+			text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+		},
+		bottlegourd: {
+			name: 'Bottle Gourd',
+			text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+		},
+		cotton: {
+			name: 'Cotton',
+			text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+		},
+		sugarcane: {
+			name: 'Sugarcane',
+			text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+		},
+	};
+
 	return res.status(200).json({
 		success: true,
 		message: 'all crop data',
@@ -222,7 +245,7 @@ app.get('/api/crops', (req, res) => {
 
 
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 9000;
 app.listen(PORT, (err) => {
 
 	if(err) {
